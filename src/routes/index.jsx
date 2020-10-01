95aea18a3bacd747287a210fe9154caa5977d06b
Matheus Flor <matheuscostaflor@gmail.com>
2020-09-30 21:26:32 -0300

    lint da cpoorra

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import ContaEstoque from '../pages/ContaEstoque';
-import HomePage from '../pages/Home';
 
 const Routes = (props) => {
     return (
